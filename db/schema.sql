c541166be60798f06fddce9aec2e95b891260df8
Danny <danny.f.gomez@gmail.com>
2024-04-23 16:55:51 -0400

    Add seed.sql data

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,27 @@
-CREATE DATABASE business_db;
-
-USE business_db;
+DROP DATABASE IF EXISTS dunder_db;
+CREATE DATABASE dunder_db;
+USE dunder_db;
 
 CREATE TABLE departments (
-    id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    job_title VARCHAR(30) NOT NULL,
+    job_title VARCHAR(75) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES departments(id)
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    manager_id INT NOT NULL,
+    manager_id INT,
     role_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(id)
     ON DELETE SET NULL
-)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,37 @@
+INSERT INTO departments (department_name)
+VALUES ("Sales"),
+    ("Customer Service"),
+    ("Finance"),
+    ("Administration"),
+    ("Human Resources"),
+    ("Warehouse");
+
+INSERT INTO roles (department_id, job_title, salary)
+VALUES (1, "Salesperson", 85000),
+    (2, "Receptionist", 80000),
+    (2, "Customer Service Specialist", 80000),
+    (3, "Head of Accounting", 260000),
+    (3, "Accountant", 250000),
+    (4, "Manager", 275000),
+    (4, "Assistant to the Regional Manager", 175000),
+    (5, "HR Representative", 160000),
+    (6, "Warehouse Foreman", 90000);
+
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
+VALUES ("Michael", "Scott", NULL, 6),
+    ("Jim", "Halpert", 1, 1),
+    ("Dwight", "Schrute", 1, 7),
+    ("Pam", "Beesly", 1, 2),
+    ("Angela", "Martin", 1, 4),
+    ("Kelly", "Kapoor", 1, 3),
+    ("Andy", "Bernard", 1, 1),
+    ("Toby", "Flenderson", NULL, 8),
+    ("Kevin", "Malone", 5, 5),
+    ("Stanley", "Hudson", 1, 1),
+    ("Oscar", "Martinez", 5, 5),
+    ("Daryll", "Philbin", NULL, 9);
+
+
+SELECT * FROM departments;
+SELECT * FROM roles;
+SELECT * FROM employees;
