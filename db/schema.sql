5704a7f8e8a78c5a035c84c199ef1757b0781fba
Danny <danny.f.gomez@gmail.com>
2024-04-23 15:54:27 -0400

    Add db items and create tables in schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE business_db;
+
+USE business_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    job_title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+    ON DELETE SET NULL
+)
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INT NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+    ON DELETE SET NULL
+)
